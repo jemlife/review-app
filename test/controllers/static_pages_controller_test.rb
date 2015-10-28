3740ebcc15aa3e3c38aa1a34152371c9082532a0
Jeremy Greenwood <jeremy.rmrr@gmail.com>
2015-10-28 12:03:02 -0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Blue Ribbons Review"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Blue Ribbons Review"
   end
 
+  test "should get contact us page" do
+  	get :contact_us
+  	assert_response :success
+  	assert_select "title", "Contact Us | Blue Ribbons Review"
+end
 end
